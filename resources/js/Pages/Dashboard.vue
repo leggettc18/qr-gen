e45e5a94b4ca2428b62cb910b2ca722d7de530e7
Christopher Leggett <chris@leggett.dev>
2022-02-26 21:33:48 +0000

    Fixes a few more dark theme issues.

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -13,7 +13,7 @@ import { Head } from "@inertiajs/inertia-vue3";
 
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="bg-white dark:bg-gray-900 overflow-hidden shadow-sm sm:rounded-lg">
           <div
             class="p-6 bg-white dark:bg-gray-700 dark:text-gray-100 border-b border-gray-200 dark:border-gray-800"
           >You're logged in!</div>
