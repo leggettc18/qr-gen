ba37d7113ed1e7f049de94bbdc12efa0cde00cce
Christopher Leggett <chris@leggett.dev>
2022-03-27 01:37:24 +0000

    👀 Fixes typo that would have impacted production and cleans up 👀
    Until now a production build from before I switched out mix for
    vite was sitting in the public folder. I deleted that.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,7 +18,7 @@
             @php
                 $manifest = json_decode(file_get_contents(public_path('dist/manifest.json')), true);
             @endphp
-            <script type="module" src="/dist/(( $manifest['resources/js/app.ts']['file'] }}"></script>
+            <script type="module" src="/dist/{{ $manifest['resources/js/app.ts']['file'] }}"></script>
             <link rel="stylesheet" href="/dist/{{ $manifest['resources/js/app.ts']['css'][0] }}">
         @else
             <script type="module" src="http://localhost:3030/@vite/client"></script>
