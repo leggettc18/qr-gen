df8f0593164e337c49b0bd6d1e80ba2b0c9e1460
Christopher Leggett <chris@leggett.dev>
2022-04-04 02:52:15 +0000

    🔧Hopefully fixes bug causing Login and Register Pages to not be found in prod🔧

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -3,13 +3,18 @@ import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import route from 'ziggy-js';
 
-import '../css/app.css'
+import '../css/app.css';
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName =
+    window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: async (name) => (await import(`./Pages/${name}.vue`)).default,
+    resolve: async (name) => {
+        const pages = import.meta.glob('./Pages/**/*.vue');
+
+        return (await pages[`./Pages/${name}.vue`]()).default;
+    },
     setup({ el, app, props, plugin }) {
         createApp({ render: () => h(app, props) })
             .use(plugin)
