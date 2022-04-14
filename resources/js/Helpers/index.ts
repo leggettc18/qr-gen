81888ba50bd9e6357f0005380ea0528f99d8ed1f
Christopher Leggett <chris@leggett.dev>
2022-04-14 02:01:42 +0000

    🧾Adds some comments explaining the debouce function🧾

--- resources/js/Helpers/index.ts
+++ resources/js/Helpers/index.ts
@@ -1,14 +1,24 @@
+// Provides an interface for a generic function type that can have any
+// arguments and any return value.
 interface FunctionWithArguments {
     (...args: any): any;
 }
 
+// Provides an interface to a Generic DeboucedFunction Type, which takes
+// any function and wraps its return type in a Promise.
 interface DebouncedFunction<F extends FunctionWithArguments> { (...args: Parameters<F>): Promise<ReturnType<F>>; }
 
+// Provides an interface for a generic DebouceReturn type, which returns
+// an array containing the DebouncedFunction (derived from the 
+// FunctionWithArguments that was supplied), and the teardown function
+// (which has no arguments and does not return anything).
 interface DebounceReturn<F extends FunctionWithArguments> extends Array<DebouncedFunction<F> | (() => void)> {
     0: (...args: Parameters<F>) => Promise<ReturnType<F>>;
     1: () => void;
 }
 
+// Takes a callback function and returns another function which runs the same
+// function after a delay (specified by timeout) and returns the value wrapped in a Promise.
 export const debounce: (<F extends FunctionWithArguments>(callback: F, timeout: number) => DebounceReturn<F>) = <F extends FunctionWithArguments>(callback: F, timeout: number) => {
     let timer: ReturnType<typeof setTimeout>;
 
